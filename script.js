0f36156bd80442becd3fb63c0995f4ecf57a7ede
ArchivexBlasich <fblasich0@gmail.com>
2025-07-18 15:50:48 -0300

    Add logic to getComputerChoice Rock or Paper or Scissors

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+
+    switch (choice) {
+        case 0:
+            console.log("rock");
+            break;
+
+        case 1:
+            console.log("paper");
+            break;
+    
+        case 2:
+            console.log("scissors");
+            break;
+
+        default:
+            break;
+    }
+}
+
+getComputerChoice();
